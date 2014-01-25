8d849ab36aaf4847f4fa65a86cf66c44dab418f2
Josh Finnie <josh@koansys.com>
2014-01-25 10:52:41 -0500

    Updated Hashing Algo for issue #4

--- test/test_hashfile.js
+++ test/test_hashfile.js
@@ -4,16 +4,23 @@ var hashfile = require('../lib/hashfile');
 
 
 describe('hashlib', function(){
+
 	it('should return proper hash of filename', function(){
-	  assert.equal('kXQrnlOq1l.json', hashfile.hash('test_of_long_file_name.json'));
+	  hashfile.hash('test/test_of_long_file_name.json', 'test_salt', function (result){
+	  	assert.equal('aNLAzajPvK.json', result);
+	  });
 	})
 
 	it('should return same file extension', function(){
-		assert.equal('PNG', hashfile.hash('test_image.PNG').split('.').pop());
+	  hashfile.hash('test/test_of_long_file_name.json', 'test_salt', function (result) {
+	  	assert.equal('json', result.split('.').pop());
+	  });
 	})
 
-	it('should return a filename of only 8 charachers', function(){
-		var file = hashfile.hash('test_of_long_file_name.json');
-		assert.equal(10, file.substr(0, file.lastIndexOf('.')).length);
+	it('should return a filename of only 10 charachers', function(){
+	  hashfile.hash('test/test_of_long_file_name.json', 'test_salt', function (result) {
+	  	assert.equal(10, result.substr(0, result.lastIndexOf('.')).length);
+	  });
 	})
+	
 })
