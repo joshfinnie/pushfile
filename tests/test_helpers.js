1c2e6271c3bc2998e66161d6ae0a25ddd9048133
Josh Finnie <josh.finnie@trackmaven.com>
2018-10-10 23:28:19 -0400

    adding tests back

---
+++ tests/test_helpers.js
@@ -0,0 +1,24 @@
+import assert from 'assert';
+
+import { hashFile } from '../src/helpers';
+
+
+describe('test hashFile', () => {
+  it('should return proper hash of filename', () => {
+    hashFile('tests/test_of_long_file_name.json', 'test_salt', (result) => {
+      assert.equal('5za6EJ8Qx9uEy5NoMLqoHdK6lpj9ZrcJ1ga.json', result);
+    });
+  });
+
+  it('should return same file extension', () => {
+    hashFile('tests/test_of_long_file_name.json', 'test_salt', (result) => {
+      assert.equal('json', result.split('.').pop());
+    });
+  });
+
+  it('should return a filename of at least 10 charachers', () => {
+    hashFile('tests/test_of_long_file_name.json', 'test_salt', (result) => {
+      assert.atLeast(result.substr(0, result.lastIndexOf('.')).length, 10);
+    });
+  });
+});
