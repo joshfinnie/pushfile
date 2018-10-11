162ee08471dc6139fd73bf5c1bb2023ad25870d3
Josh Finnie <josh.finnie@trackmaven.com>
2018-10-10 22:58:07 -0400

    testing out an update

---
+++ src/pushfile.js
@@ -0,0 +1,31 @@
+import chalk from 'chalk';
+import figlet from 'figlet';
+import commander from 'commander';
+
+import { version } from '../package.json';
+import pushFile, { createConfig } from './helpers';
+
+console.log(
+  chalk.red(
+    figlet.textSync('PushFile!', { horizontalLayout: 'full' }),
+  ),
+);
+
+commander
+  .version(version)
+  .usage('[options] <file ...>')
+  .option('-u, --unique', 'Gives a unique hash for uploaded file.')
+  .option('-c, --configure', 'Create a configuration file.')
+  .option('-v, --version', 'Prints Version');
+
+commander.parse(process.argv);
+
+const argsLength = commander.args.length;
+
+if (commander.configure) {
+  createConfig();
+} else if (argsLength > 0) {
+  pushFile(commander.args[0], commander.unique);
+} else if (argsLength <= 0) {
+  console.log('no filename...');
+}
