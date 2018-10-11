4e06c43410d12624ff68f8c8808cc48bc2ff1973
Josh Finnie <josh.finnie@trackmaven.com>
2018-10-11 00:10:25 -0400

    fixing path to json

--- test/test_helpers.js
+++ test/test_helpers.js
@@ -6,19 +6,19 @@ const { assert } = chai;
 
 describe('test hashFile', () => {
   it('should return proper hash of filename', () => {
-    hashFile('tests/test_of_long_file_name.json', 'test_salt', (result) => {
+    hashFile('test/test_of_long_file_name.json', 'test_salt', (result) => {
       assert.equal('5za6EJ8Qx9uEy5NoMLqoHdK6lpj9ZrcJ1ga.json', result);
     });
   });
 
   it('should return same file extension', () => {
-    hashFile('tests/test_of_long_file_name.json', 'test_salt', (result) => {
+    hashFile('test/test_of_long_file_name.json', 'test_salt', (result) => {
       assert.equal('json', result.split('.').pop());
     });
   });
 
   it('should return a filename of at least 10 charachers', () => {
-    hashFile('tests/test_of_long_file_name.json', 'test_salt', (result) => {
+    hashFile('test/test_of_long_file_name.json', 'test_salt', (result) => {
       assert.isTrue(result.substr(0, result.lastIndexOf('.')).length > 10);
     });
   });
